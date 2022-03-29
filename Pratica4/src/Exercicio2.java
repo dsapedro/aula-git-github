d8a11cb1b1a0651787bff412933ec616692c6c36
Pedro dos Santos Assunção <pedro251502@gmail.com>
2022-03-29 20:57:15 -0300

    att

---
+++ Pratica4/src/Exercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+	double lado1,lado2,lado3;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Digite o valor do primeiro lado: ");
+	lado1 = input.nextDouble();
+	System.out.println("Digite o valor do segundo lado: ");
+	lado2 = input.nextDouble();
+	System.out.println("Digite o valor do terceiro lado: ");
+	lado3 = input.nextDouble();
+	
+	if(lado1==lado2 && lado2==lado3) {
+		System.out.println("Triângulo Equilátero.");
+	}else if(lado1==lado2 || lado2==lado3 || lado1==lado3){
+		System.out.println("Triângulo Isóceles.");
+	}else {
+		System.out.println("Triângulo Escaleno.");
+	}
+
+	}
+
+}
