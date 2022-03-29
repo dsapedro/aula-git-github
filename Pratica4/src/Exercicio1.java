71deb0fd9cb4c3ebe9a200634a3c73fc6dd9a556
Pedro dos Santos Assunção <pedro251502@gmail.com>
2022-03-29 20:44:43 -0300

    att

--- Pratica4/src/Exercicio1.java
+++ Pratica4/src/Exercicio1.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nota1, nota2, media;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Digite a primeira nota: ");
+		nota1 = input.nextInt();
+		System.out.println("Digite a segunda nota: ");
+		nota2 = input.nextInt();
+
+		media = (nota1 + nota2) / 2;
+
+		if (media >= 7) {
+			System.out.println("Aprovado!");
+		}else {
+			System.out.println("Reprovado!");
+		}
 
 	}
 
