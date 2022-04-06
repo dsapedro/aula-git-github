c0d812f8bea1035a935d625b39fee43db16fa648
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-04-06 08:22:26 -0300

    Update Exercicio3.java

--- Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
+++ Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio3;
 
-/**
- *
- * @author 42024019
- */
 public class Exercicio3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         double canal, canal4, canal5, canal7, canal12, pessoas;
     
