cf2a85c962d7e8758a3674b4fa8dda892b22bf2b
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-04-06 12:30:27 -0300

    Update Exercicio3.java

--- Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
+++ Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
@@ -4,7 +4,7 @@ package exercicio3;
 public class Exercicio3 {
 
     public static void main(String[] args) {
-        double canal, canal4, canal5, canal7, canal12, pessoas, pessoasTotal;
+        double canal=0, canal4=0, canal5=0, canal7=0, canal12=0, pessoas=0, pessoasTotal=0;
     
     Scanner input = new Scanner(System.in);
     
@@ -15,12 +15,16 @@ public class Exercicio3 {
     
     while(canal!=0){
         pessoasTotal=pessoas+pessoasTotal;
-        if(canal == 4){canal4+=pessoas;}
-        else if(canal == 5){canal5+=pessoas;}
-        else if(canal == 7){canal7+=pessoas;}
-        else if(canal == 12){canal12+=pessoas;}
+        if(canal == 4){canal4=canal4+pessoas;}
+        else if(canal == 5){canal5=canal5+pessoas;}
+        else if(canal == 7){canal7=canal7+pessoas;}
+        else if(canal == 12){canal12=canal12+pessoas;}
+        System.out.println("Informe o número do canal: ");
+    canal=input.nextDouble();
+    System.out.println("Informe o número de pessoas: ");
+    pessoas=input.nextDouble();
     }
-    System.out.println("Canal 4: "+(canal4*100/pessoas)+"\nCanal 5: "+(canal5*100/pessoas)+"\nCanal 7: "+(canal7*100/pessoas)+"\nCanal 12: "+(canal12*100/pessoas));
+    System.out.println("Canal 4: "+(canal4*100/pessoasTotal)+"\nCanal 5: "+(canal5*100/pessoasTotal)+"\nCanal 7: "+(canal7*100/pessoasTotal)+"\nCanal 12: "+(canal12*100/pessoasTotal));
     }
     
 }
