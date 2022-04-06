8f614d1d4e341f60bac5cbd3e9f35c25ec0edc2a
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-04-06 08:31:41 -0300

    Update Exercicio3.java

--- Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
+++ Pratica5/Exercicio3/src/exercicio3/Exercicio3.java
@@ -4,7 +4,7 @@ package exercicio3;
 public class Exercicio3 {
 
     public static void main(String[] args) {
-        double canal, canal4, canal5, canal7, canal12, pessoas;
+        double canal, canal4, canal5, canal7, canal12, pessoas, pessoasTotal;
     
     Scanner input = new Scanner(System.in);
     
@@ -13,7 +13,14 @@ public class Exercicio3 {
     System.out.println("Informe o número de pessoas: ");
     pessoas=input.nextDouble();
     
-    while(canal!=0){}
+    while(canal!=0){
+        pessoasTotal=pessoas+pessoasTotal;
+        if(canal == 4){canal4+=pessoas;}
+        else if(canal == 5){canal5+=pessoas;}
+        else if(canal == 7){canal7+=pessoas;}
+        else if(canal == 12){canal12+=pessoas;}
+    }
+    System.out.println("Canal 4: "+(canal4*100/pessoas)+"\nCanal 5: "+(canal5*100/pessoas)+"\nCanal 7: "+(canal7*100/pessoas)+"\nCanal 12: "+(canal12*100/pessoas));
     }
     
 }
