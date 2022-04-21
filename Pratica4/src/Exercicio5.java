25b5a4d2fbf621c269b6d5bb40d28067e1ed5024
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-04-21 15:43:23 -0300

    Update Exercicio5.java

--- Pratica4/src/Exercicio5.java
+++ Pratica4/src/Exercicio5.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Exercicio5 {
 
 	public static void main(String[] args) {
-	
+		double x;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Informe o valor de x: ");
+		x = input.nextDouble();
+		if (x < -2) {
+			System.out.println("Resultado: " + (2 * x + 2));
+		} else if (x >= -2 && x < 3) {
+			System.out.println("Resultado: 3");
+		} else {
+			System.out.println("Resultado: " + (-x));
+		}
 
 	}
 
