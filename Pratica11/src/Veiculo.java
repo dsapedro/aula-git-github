7c5790421c468a565cb3880882f73a67bc36ccfe
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-05-03 21:31:20 -0300

    Add files via upload

---
+++ Pratica11/src/Veiculo.java
@@ -0,0 +1,37 @@
+
+public class Veiculo {
+	private int numeroChassi, x = 0, i = 0;
+	private String placa;
+
+	public Veiculo() {
+		numeroChassi = 0;
+		placa = "";
+	}
+
+	Veiculo veiculo[] = new Veiculo[3];
+
+	public void cadastrar() {
+		while (x != 1) {
+			Veiculo veiculo[] = new Veiculo[i];
+
+		}
+
+	}
+
+	public int getChassi() {
+		return (numeroChassi);
+	}
+
+	public void setChassi(int setchassi) {
+		numeroChassi = setchassi;
+	}
+
+	public String getPlaca() {
+		return (placa);
+	}
+
+	public void setPlaca(String setplaca) {
+		placa = setplaca;
+	}
+
+}
