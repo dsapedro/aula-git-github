efaf80d4ac835fd6a5f625587939138f09b33fee
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-05-17 12:37:34 -0300

    Update Exercicio1.java

--- Pratica3/src/Exercicio1.java
+++ Pratica3/src/Exercicio1.java
@@ -10,14 +10,14 @@ public class Exercicio1 {
 	System.out.println("Digite a primeira nota: ");
 	nota1 = input.nextDouble();
 	
-	System.out.println("Digite a primeira nota: ");
+	System.out.println("Digite a segunda nota: ");
 	nota2 = input.nextDouble();
 	
-	System.out.println("Digite a primeira nota: ");
+	System.out.println("Digite a terceira nota: ");
 	nota3 = input.nextDouble();
 	
 	media = (nota1 + nota2 + nota3) / 3; 
-	System.out.printf("A média é: %.2f",media);
+	System.out.printf("A mÃ©dia Ã©: %.2f",media);
 	}
 
 }
